501e874df9e4d0e1598867f56f4e196c036ed715
jgodwin10 <easypeasy725@gmail.com>
2024-04-10 23:06:23 +0100

    Deploy

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import { UserMiddleware } from "./middleware/AuthMiddleware.js";
 import router from "./Router/userRoutes.js";
-import path from 'path'
-
-
+import path from "path";
+import cors from "cors";
 
 dotenv.config();
 
@@ -13,22 +12,23 @@ mongoose.connect(process.env.MONGODB).then(() => {
   console.log("connected");
 });
 
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 
+const corsOptions = "";
 
 const app = express();
-
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const Port = process.env.PORT || 3000;
 
 app.use(router);
 
-app.use(express.static(path.join(__dirname, 'frontend/dist')))
+app.use(express.static(path.join(__dirname, "frontend/dist")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.use(UserMiddleware);
 
