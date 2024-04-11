5feed9866854e2043576204faa33442e8221c7e1
jgodwin10 <easypeasy725@gmail.com>
2024-04-11 07:12:17 +0100

    port

--- frontend/src/App/apiSlice.jsx
+++ frontend/src/App/apiSlice.jsx
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const authApi = createApi({
   reducerPath: "auth",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:10000" }),
   tagTypes: ["Posts"],
   endpoints: (build) => ({
     register: build.mutation({
