6f43e2bd51a68081a3e28af4b398904f40bb398d
jgodwin10 <144743903+jgodwin10@users.noreply.github.com>
2024-05-01 14:00:07 +0100

    Update apiSlice.jsx

--- frontend/src/App/apiSlice.jsx
+++ frontend/src/App/apiSlice.jsx
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const authApi = createApi({
   reducerPath: "auth",
-  baseQuery: fetchBaseQuery({ baseUrl: "/api" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://challenges-three-chi.vercel.app/api" }),
   tagTypes: ["Posts"],
   endpoints: (build) => ({
     register: build.mutation({
