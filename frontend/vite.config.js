e720c00fb476606d3e495e30c16acc25a6048042
jgodwin10 <144743903+jgodwin10@users.noreply.github.com>
2024-05-01 14:04:21 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,10 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        secure: false,
-      },
+    
     },
   },
   plugins: [react()],
